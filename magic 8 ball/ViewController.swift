43218e76346ebf66f906f766f23f37f8edc81e22
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-07-10 10:37:22 +0400

    Initial Commit

---
+++ magic 8 ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic 8 ball
+//
+//  Created by Farid Hamzayev on 10.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
